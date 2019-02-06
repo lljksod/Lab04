ca2a24b8d10a4097a3265c6cfb7d263f6b2f98d4
Richie Burch <rburch@gmail.com>
2019-02-05 23:01:28 -0600

    Added setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
    to onCreate. Fixed issue with orientation refusing to change.

--- app/src/main/java/com/rburch/lab03/MainActivity.java
+++ app/src/main/java/com/rburch/lab03/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rburch.lab03;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
         if (savedInstanceState != null){
             praiseCount = savedInstanceState.getInt(PRAISE_COUNTER);
         }
